f819a609b32cc2a788d0c902e3bb22dffffc91e0
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-03-02 19:53:35 -0500

    done api routes user model

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,9 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
-//  new Sequelize() function accepts the database name, MySQL username, and MySQL password (respectively) as parameters,
-// create connection to our database, pass in your MySQL information for username and password
+
+// create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  // we also pass configuration settings
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const {
-  User
-} = require('../../models');
-//  GER /api/users
+const { User } = require('../../models');
+
+// get all users
 router.get('/', (req, res) => {
-  // Access our User model and run .findAll() method)
-  User.findAll()
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -13,18 +13,79 @@ router.get('/', (req, res) => {
     });
 });
 
-// GET /api/users
-router.get('/', (req, res) => {});
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-// POST /api/users
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-//  PUT /api/users/1
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-// DELETE /api/users/1
-router.delete('/:id', (req, res) => {});
+  // pass in req.body instead to only update what's passed through
+  User.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
-
-//  These endpoints for the server are going to be accessible at the /api/users URL
