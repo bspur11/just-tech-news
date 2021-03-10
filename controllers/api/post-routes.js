782c11bf3efeec001d1f998110c8194ff2d82cdd
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-03-09 22:44:25 -0500

    add handlebars

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,34 +4,33 @@ const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
-  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
       'post_url',
       'title',
       'created_at',
-      //  .literal() that allows us to run regular SQL queries from within the Sequelize method-based queries
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    order: [['created_at', 'DESC']],
     include: [
-       // include the Comment model here:
-   {
-    model: Comment,
-    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-    include: {
-      model: User,
-      attributes: ['username']
-    }
-  },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
       }
     ]
   })
-    .then(dbPostData => res.json(dbPostData))
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      res.render('homepage', dbPostData[0]);
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
---
+++ controllers/home-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      console.log(dbPostData)
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+// res.render() and specify which template we want to use. In this case, we want to rende
+// . The res.render() method can accept a second argument, an object, which includes all of the data you want to pass to your template
+
+module.exports = router;
